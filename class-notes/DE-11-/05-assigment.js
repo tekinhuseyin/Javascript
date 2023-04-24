2dbd87e9910e027b4649f07ed3ca9758b61e10bf
huseyintekin <huseyintekin10@gmail.com>
2023-04-24 09:37:34 +0200

    dom.manipülation

---
+++ class-notes/DE-11-/05-assigment.js
@@ -0,0 +1,22 @@
+// function changeImage(x,image)
+// {
+//     if(x == 1)
+//  Img.
+// {
+//     image.src = "image/aslan1jpeg";
+// }
+// if (x == 2)
+// {
+//     image.src = "image/aslan2";
+// }
+// }
+
+
+// const audio = new Audio("image/retro-ringtone.mp3");
+// const buttons = document.querySelectorAll("button");
+
+// buttons.forEach(button => {
+//   button.addEventListener("Call", () => {
+//     audio.play();
+//   });
+// });
