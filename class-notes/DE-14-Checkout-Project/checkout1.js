5f351ce848636f0d180275d006debfbc376dd4bc
huseyintekin <huseyintekin10@gmail.com>
2023-04-27 23:25:15 +0200

    checkout

---
+++ class-notes/DE-14-Checkout-Project/checkout1.js
@@ -0,0 +1,12 @@
+//* ======================================================================
+//*                 Checkout Page Solution
+//*  map filter, dest,spread==============================================
+//!table da kullanılacak değişkenler
+const kargo = 15.0;
+const vergi = 0.18;
+
+let sepettekiler = [
+  { name: "Vintage Backpack", price: 34.99, adet: 1, img: "./img/photo1.png" },
+  { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
+  { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
+];
