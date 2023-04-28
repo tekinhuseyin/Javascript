d318bdca7bfa5d076dcb798beb2ab8423a245f42
huseyintekin <huseyintekin10@gmail.com>
2023-04-28 23:22:24 +0200

    checkout

--- class-notes/DE-14-Checkout-Project/checkout1.js
+++ class-notes/DE-14-Checkout-Project/checkout1.js
@@ -10,3 +10,15 @@ let sepettekiler = [
   { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
   { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
 ];
+
+sepettekiler.forEach((ürün,i)=>{
+
+  document.querySelectorAll("img")[i].src=ürün.img
+  document.querySelectorAll("h5")[i].textContent=ürün.name
+  document.querySelectorAll(".h5")[i].textContent=ürün.price
+document.querySelectorAll(".indirim-price")[i].textContent=(ürün.price*0.7).toFixed(2)
+
+document.querySelectorAll(".ürün-toplam")[i].textContent = (ürün.price * 0.7*ürün.adet).toFixed(2)
+})
+
+
