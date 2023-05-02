df719a6bfc5b33e78f5c132450f6d4dcb5ddde86
huseyintekin <huseyintekin10@gmail.com>
2023-05-02 15:56:11 +0200

    promise

--- class-notes/DE.15.Async/2-promise.js
+++ class-notes/DE.15.Async/2-promise.js
@@ -30,7 +30,6 @@
 //? then() ve catch() metotlari promise dondururler.
 //? Zincirleme olarak kullanilabilirler.
 
-
 console.log("promise başlıyor");
 const person = { name: "Ali", surname: "Gel" };
 
@@ -44,3 +43,53 @@ new Promise((resolve, reject) => {
   .catch((error) => {
     console.log(error);
   });
+//* Example-2: TEA TIME
+//* -----------------------------------------------------------------------
+//* 1. su kaynayana kadar bekleyin -> kettle -> work or failed
+//* 2. çay ekle --> çay var ya da yok
+//* 3. demlenmesini bekle
+
+//!ilk then de sıkıntı varsa direk catch e düşer
+const brewTea = () => {
+  boil()
+    .then((status1) => {
+      console.log(status1);
+      return addTea();
+    })
+    .then((status2) => {
+      console.log(status2);
+
+      return "Çay hazır. Afiyet olsun";
+    }).catch((err) => console.log(err));
+};
+const boil = () => {
+  return new Promise((resolve, reject) => {
+    const boiled = Math.floor(Math.random() * 5); //!0 tutarsa false
+    if (boiled) {
+      resolve("Su Kaynadı");
+    } else {
+      reject(new Error("Kettle Başarısız"));
+    }
+  });
+};
+
+const addTea = () => {
+  return new Promise((resolve, reject) => {
+    const teaAvalaible = Math.floor(Math.random() * 3);
+    if (teaAvalaible) {
+      resolve("Çay Eklendi");
+    } else {
+      reject(new Error("Çay kalmamış"));
+    }
+  });
+};
+
+ brewTea();
+
+ //?----------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//*daha tercih edilenler
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
