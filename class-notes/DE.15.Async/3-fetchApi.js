9f5f3b32220fab3d2988213e5516110eac4def54
huseyintekin <huseyintekin10@gmail.com>
2023-05-02 15:55:41 +0200

    fetch

--- class-notes/DE.15.Async/3-fetchApi.js
+++ class-notes/DE.15.Async/3-fetchApi.js
@@ -1,11 +1,10 @@
 //* ===========================================================
-//*                          3- FETCH API
+//*                3- FETCH API
 //*=============================================================
 
 //? Dis kaynaklardan veri getirmek icin kullanilan basit bir arabirimdir.
 //? Ag istekleri yapmamiza ve cevaplari yonetebilmemize olanak saglar.
-//? Javascript ortaminda en cok kullanilan Asenkron islem orneklerinin basinda
-//? gelmektedir.
+//? Javascript ortaminda en cok kullanilan Asenkron islem orneklerinin basinda gelmektedir.
 
 //? fetch() fonksiyonu veri getirmek istediginiz kaynagin yolunu gosteren zorunlu
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
@@ -16,20 +15,28 @@ const veriGetir = () => {
   fetch("https://api.github.com/users")
     .then((res) => {
       if (!res.ok) {
-        throw new Error("url de hata var");
+         throw new Error("url de hata var");
+        console.log("hata");
       }
       return res.json();
     })
     .then((veri) => ekranaBastir(veri));
 };
-veriGetir();
+
+document.querySelector("button").onclick=()=>{
+    veriGetir()
+}
+
+
 
 const ekranaBastir = (veri) => {
   veri.forEach((eleman) => {
     document.querySelector("section").innerHTML += `
         
         <h1 class="mt-4"> NAME:  <span class="text-danger">${eleman.login}</span>  </h1>
-        
+        <img src=${eleman.avatar_url} width="50%"/>
+
+        <h3> NODE-ID: ${eleman.node_id} </h3>
         `;
   });
 };
